ff0f2f1b32933493439449a2d51da7a9a7c9a965
Frédéric Woelffel <woelffel.f@gmail.com>
2015-08-04 14:49:21 +0200

    Fixed main navbar's behaviour

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,40 @@
-$("#navbar").on("hidden.bs.collapse", function(){
-    $(".navbar-header").toggleClass("navbar-collapse-expanded")
+function checkSize() {
+    if($(".navbar-header").css("float") == "none") {
+        $(document).trigger("size.sm");
+    }
+    else {
+        $(document).trigger("size.md");
+    }
+}
+
+// ---------------------------
+
+$(document).ready(function() {
+
+    $(document).on("size.md", function () {
+        $("#main-navbar").removeClass("navbar-collapse-expanded");
+        $("#navbar-links").removeClass("in");
+        $("#navbar-links").attr("aria-expanded", false);
+    });
+
 });
-$("#navbar").on("show.bs.collapse", function(){
-    $(".navbar-header").toggleClass("navbar-collapse-expanded");
+
+$("#main-navbar #navbar-links").on("hidden.bs.collapse", function () {
+    $("#main-navbar").toggleClass("navbar-collapse-expanded")
 });
+
+$("#main-navbar #navbar-links").on("show.bs.collapse", function () {
+    $("#main-navbar").toggleClass("navbar-collapse-expanded");
+});
+
+// ---------------------------
+
+$("#main-navbar").affix({
+    offset: {
+        top: function() {
+            return $("#main-header").height() - $("#navbar-links").height();
+        }
+    }
+});
+
+$(window).resize(checkSize);
